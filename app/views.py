0508ccfbed681b39ca1e085e3f76973e6927d589
Amir <amir.mofakhar@birdstep.com>
2015-07-13 11:36:53 +0200

    working

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, Flask!"
-	
+	#return "Hello, Flask!"
+	pass_param= {'var1':'A'}
+	return render_template('index.html',
+							title='Home',
+							param=pass_param)
